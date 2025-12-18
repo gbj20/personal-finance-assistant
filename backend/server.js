8f9f216160ccb8bca3a957cf9f4441828a920b4a
Gayatri Jadhav <gayatrijadhav0219@gmail.com>
2025-12-18 15:22:56 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -160,7 +160,7 @@ app.post("/api/change-password", authenticateToken, (req, res) => {
 });
 
 // ================== START SERVER ==================
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
